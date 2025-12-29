4b3ac6d20e2f4aa884559de199cbcd4eaa712a86
AatishPatel <100529340+aatishpatel5@users.noreply.github.com>
2025-12-29 15:27:29 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ app.set("trust proxy", 1);
 const port = process.env.PORT || 5000
 
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://authentication-1-frontend.onrender.com",
     credentials: true
 }))
 
