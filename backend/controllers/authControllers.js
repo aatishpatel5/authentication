d2182a9d95a7712f818ad7a533c09f5221d0d0bb
AatishPatel <100529340+aatishpatel5@users.noreply.github.com>
2025-12-30 09:14:50 +0530

    Update authControllers.js

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -110,8 +110,8 @@ export const verifyOtpAndSignup = async (req, res) => {
      res.cookie("token", token, {
   httpOnly: true,
   secure: true ,// Production me TRUE, Localhost par FALSE
-  sameSite: "none" // Cross-site cookie ke liye 'none' zaroori hai
-  maxAge: 7 * 24 * 60 * 60 * 1000,
+  sameSite: "none", // Cross-site cookie ke liye 'none' zaroori hai
+  maxAge: 7 * 24 * 60 * 60 * 1000
 });
 
     return res.status(200).json(user);
